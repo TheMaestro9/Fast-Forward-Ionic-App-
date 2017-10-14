bc959c599ae3da7fc5d9cb31f7018aa5ece09267
TheMaestro9 <walidmoussa995@gmail.com>
2017-10-14 10:19:42 +0200

    some changes in the register page

--- src/pages/registerform/registerform.ts
+++ src/pages/registerform/registerform.ts
@@ -28,9 +28,8 @@ export class Registerform {
   month;
   year;
   major="";
-loader;
+  loader;
   age=new Date();
-  expo;
   constructor(platform:Platform,public navCtrl: NavController,public DS: DataService,  public http: Http,public navParams: NavParams,public alertCtrl: AlertController,public plt: Platform,private network: Network, private loadingCtrl: LoadingController, private store: Storage) {
 
 
@@ -46,17 +45,17 @@ loader;
     });
 
     
-    platform.ready().then(()=>{
+    // platform.ready().then(()=>{
       
-              platform.registerBackButtonAction(() =>{
+    //           platform.registerBackButtonAction(() =>{
       
-                if(this.navCtrl.canGoBack()){
-                  this.navCtrl.pop();
-                }
-              });
+    //             if(this.navCtrl.canGoBack()){
+    //               this.navCtrl.pop();
+    //             }
+    //           });
       
       
-            });
+    // });
   
     this.name = navParams.get("name");
   // this.getage = navParams.get("age");
@@ -66,7 +65,6 @@ loader;
     if (plt.is('ios')) {
       this.ios = true;
     }
-  this.expo=false;
   }
   setDate(event){
     this.age=event;
@@ -82,38 +80,37 @@ this.age.setFullYear(this.year,this.month,this.day);
 
     if (this.name != "" && this.email != "" && pass != "" && school != "" && this.age !=this.localDate && phone != "" ){
       console.log("not null");
-      if(phone>1299999999 && phone<1000000000)this.showAlert("Enter a valid phone number");
+      if(phone>1299999999 && phone<1000000000)
+        this.showAlert("Enter a valid phone number");
       else{
         this.presentLoading() ;
-     let user={
-        
-        degree:this.degree,
-        user_name:this.name,
-        user_email:this.email,
-        birth_date:this.age.toISOString(),
-        password:pass,
-        school:school,
-        phone_no:phone,
-        promo_code:promo,
-        interests:this.interests,
-        major:this.major,
-        expo:this.expo
-        
+        let user={
+          
+          degree:this.degree,
+          user_name:this.name,
+          user_email:this.email,
+          birth_date:this.age.toISOString(),
+          password:pass,
+          school:school,
+          phone_no:phone,
+          promo_code:promo,
+          interests:this.interests,
+          major:this.major        
         }
         
 
 
 
 
-this.http.post("https://ffserver.eu-gb.mybluemix.net/register3", user).subscribe(data => {
-  
-  var res = JSON.parse(data['_body']);
-  this.setresponse(res);
-  if (this.expo)localStorage.setItem('expo','1');
-  this.dismissLoading();
-});
+      this.http.post("https://ffserver.eu-gb.mybluemix.net/register3", user).subscribe(data => {
+        
+        var res = JSON.parse(data['_body']);
+        this.setresponse(res);
+        this.dismissLoading();
+      });
 
-    }}
+    }
+  }
 
     else {
 
@@ -131,52 +128,35 @@ this.http.post("https://ffserver.eu-gb.mybluemix.net/register3", user).subscribe
     });
     alert.present();
   }
+
   setresponse(value) {
-
     this.check = value;
-
     if (this.check.result == false) {
       this.showAlert(this.check.msg);
-
     }
     else if(this.check.result==true){
       this.store.set('user_id', this.check.user_id);
-
-     
-      this.navCtrl.setRoot(TutorialPage);
-                  
-          }
-          
-          
-
-      
-
+      this.navCtrl.setRoot(TutorialPage);             
+     }      
   }
 
 
 
   setdata(response) {
-
     this.name = response.name;
     this.email = response.email;
     this.age = response.birthday;
-
-
   }
 
   presentLoading() {
     this. loader = this.loadingCtrl.create({
       content: "Please wait...",
-      
     });
    this. loader.present();
   }
 
   dismissLoading() {
-
-
-this.loader.dismiss();
-
+    this.loader.dismiss();
   }
 
 }
