b1d6e99230e932765ce3fd27682ccbba0d0e1f7e
osamafathi <osama_ofs22@hotmail.com>
2017-10-16 13:31:48 +0200

    change Mo4's price color , generate an alert in edit profile page

--- src/pages/edit-profile/edit-profile.ts
+++ src/pages/edit-profile/edit-profile.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IonicPage, AlertController, NavController, NavParams } from 'ionic-angular';
+import { Profile } from "../profile/profile";
 import {Http} from '@angular/http';
 
 
@@ -10,28 +11,44 @@ import {Http} from '@angular/http';
 export class EditProfilePage {
   navdata:any={};
   
-    constructor(public navCtrl: NavController, public navParams: NavParams ,public http: Http) {
+    constructor(public alertController: AlertController,public navCtrl: NavController, public navParams: NavParams ,public http: Http) {
       console.log(this.navParams);
       this.navdata=this.navParams;        
     }
+   
     edit_user_info(email,phone,school,degree,name){
-                this.navdata.data.user_email=email;
-                this.navdata.data.phone_no=phone;
-                this.navdata.data.school=school;
-                this.navdata.data.degree=degree;
-                this.navdata.data.user_name=name;
+	
+							let confirm = this.alertController.create({
+								title : 'Edit Profile',
+                message:'Are you sure you to update your data ?',
+                buttons: [
+                  { 
+                    text:'Yes',
+                    handler:() =>{
+  
+                     this.navdata.data.user_email=email;
+                     this.navdata.data.phone_no=phone;
+                     this.navdata.data.school=school;
+                     this.navdata.data.degree=degree;
+                     this.navdata.data.user_name=name;
+                   
+                 console.log("data sent :" ,this.navdata.data);
+       
+                 this.http.post("https://ffserver.eu-gb.mybluemix.net/edit-user",this.navdata.data).subscribe(data => {
+                   var res = JSON.parse(data['_body']);
+                   // this.user_simulations=res;
+                   console.log("et3'yar");
+                   console.log(res);
+                   // this.loading=false;
+                 });
+                this.navCtrl.pop();
+                }
+                },
+                {text : 'No' }
+              ]
+              });
+              confirm.present(); 
+              console.log(this.navdata.data);
               
-            console.log("data sent :" ,this.navdata.data);
-  
-            this.http.post("https://ffserver.eu-gb.mybluemix.net/edit-user",this.navdata.data).subscribe(data => {
-              var res = JSON.parse(data['_body']);
-              // this.user_simulations=res;
-              console.log("et3'yar");
-              console.log(res);
-              // this.loading=false;
-            });
-  
-          
-    }  
-  
-  }
+	}    
+}
