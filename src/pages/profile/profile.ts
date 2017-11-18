345f5d31dba9a8b5000eb76a9f372f6592149271
TheMaestro9 <walidmoussa995@gmail.com>
2017-11-18 19:00:34 +0200

    some edits for the feedback

--- src/pages/profile/profile.ts
+++ src/pages/profile/profile.ts
@@ -3,16 +3,16 @@ import { NavController, NavParams } from 'ionic-angular';
 import { AlertController } from 'ionic-angular';
 import { LoadingController } from 'ionic-angular';
 import { ActionSheetController } from 'ionic-angular'
-import {Http} from '@angular/http';
-import {EditProfilePage } from "../edit-profile/edit-profile";
-import {LoginPage} from'../login/login';
-import { Facebook} from '@ionic-native/facebook';
+import { Http } from '@angular/http';
+import { EditProfilePage } from "../edit-profile/edit-profile";
+import { LoginPage } from '../login/login';
+import { Facebook } from '@ionic-native/facebook';
 import { Storage } from '@ionic/storage';
 import { App } from 'ionic-angular';
-import {FileChooser } from 'ionic-native'; 
+import { FileChooser } from 'ionic-native';
 import { AngularFireAuth } from 'angularfire2/auth';
-import {DataService} from '../../providers/data-service';
-import {Observable} from 'rxjs/Rx';
+import { DataService } from '../../providers/data-service';
+import { Observable } from 'rxjs/Rx';
 import { CompanyPage } from '../company/company';
 @Component({
 	selector: 'page-profile',
@@ -20,93 +20,117 @@ import { CompanyPage } from '../company/company';
 	providers: [AngularFireAuth]
 })
 export class Profile {
-	user_info:any={};
-	user_simulations:any=[];
-	refresher:any;
-	nativePath: any; 
+	user_info: any = {};
+	user_simulations: any = [];
+	refresher: any;
+	nativePath: any;
 	StartDate;
-	check:any;
+	check: any;
 	nowDate = new Date();
 	diffDays;
 	diffhours;
 	diffmins;
 	diffsecs;
 	user_id;
-    timer;
-	
-	constructor(private app:App,public navCtrl: NavController, navParams: NavParams,private DS:DataService,public http: Http, public alertCtrl:AlertController,public loadingCtrl: LoadingController,public actionSheetCtrl: ActionSheetController,private fb: Facebook,private store: Storage, public afa :AngularFireAuth) {
-	
+	timer;
 
+	constructor(private app: App, public navCtrl: NavController,
+		navParams: NavParams, private DS: DataService, public http: Http,
+		public alertCtrl: AlertController, public loadingCtrl: LoadingController,
+		public actionSheetCtrl: ActionSheetController, private fb: Facebook,
+		private store: Storage, public afa: AngularFireAuth) {
+
+		this.CheckAccepted();
+			
 		this.store.get('user_id').then((val) => {
-	
-			  
-			http.get("https://ffserver.eu-gb.mybluemix.net/user_info?id="+val).subscribe(data => {
+
+
+			http.get("https://ffserver.eu-gb.mybluemix.net/user_info?id=" + val).subscribe(data => {
 				var res = JSON.parse(data['_body']);
-				this.user_info=res;
+				this.user_info = res;
 				console.log(this.user_info);
 				//this.loading=false;
 			});
-			
-			http.get("https://ffserver.eu-gb.mybluemix.net/user_simulations?id="+val).subscribe(data => {
+
+			http.get("https://ffserver.eu-gb.mybluemix.net/user_simulations?id=" + val).subscribe(data => {
 				var res = JSON.parse(data['_body']);
-				this.user_simulations=res;
+				this.user_simulations = res;
 				console.log(this.user_simulations);
 				//console.log("STAT",this.user_simulations[0].status);
 				//this.loading=false;
 			});
-			});
 
-	}	
-	GoToCompany(index,simulation){
-		this.navCtrl.push(CompanyPage,{co_id:simulation.company_id});
+		});
+
+	}
+
+	CheckAccepted() {
+		var acceptedDates = [];
+		this.store.get('Accepted').then((val) => {
+			if (val != null)
+				acceptedDates = val;
+		
+		console.log("hello" , val ) ; 
+			// for (let simulation in this.user_simulations) {
+			acceptedDates = [ 1 , 2  , 3] ; 
+			this.store.set('Accepted', acceptedDates);
+			
+			// }
+		});
+
+
+
+	}
+	GoToCompany(index, simulation) {
+		this.navCtrl.push(CompanyPage, { co_id: simulation.company_id });
 		console.log(this.user_simulations[index].company_id);
 	}
-	
-	EditProfile(){
+
+	EditProfile() {
 		console.log("sha3'aaaaal");
 		this.navCtrl.push(EditProfilePage, this.user_info);
 	}
-	
-	removeSimulation(sim){
-		
-		
-		this.store.get('user_id').then((val) => {
-		console.log("selected", sim )  ;
-		console.log("all",this.user_simulations)  ;
-		
-		console.log("index", this.user_simulations.indexOf(sim) )  ;
-					this.http.get("https://ffserver.eu-gb.mybluemix.net/user_delete_simulation?user_id="+val+"&simulation_id="+sim.simulation_date_id).subscribe(data => {
-					//	var res = JSON.parse(data['_body']);
-						console.log(data['_body']);
-						//this.loading=false;
-					});
-					console.log("user_id",val)
-				});
 
-		this.user_simulations.splice(this.user_simulations.indexOf(sim),1);
-		
-	  }
-	
-	
-	
-	
-	edit_user_info(type){
-		let value={};
-		switch(type){
-			case "name":value = this.user_info.user_name;break;			
-			case "email":value = this.user_info.user_email;break;
-			case "phone":value = this.user_info.phone_no;break;
-			case "school":value = this.user_info.school;break;
-			case "degree":value = this.user_info.degree;break;
+	removeSimulation(sim) {
+
+
+		this.store.get('user_id').then((val) => {
+			console.log("selected", sim);
+			console.log("all", this.user_simulations);
+
+			console.log("index", this.user_simulations.indexOf(sim));
+			this.http.get("https://ffserver.eu-gb.mybluemix.net/user_delete_simulation?user_id=" + val + "&simulation_id=" + sim.simulation_date_id).subscribe(data => {
+				//	var res = JSON.parse(data['_body']);
+				console.log(data['_body']);
+				//this.loading=false;
+			});
+			console.log("user_id", val)
+		});
+
+		this.user_simulations.splice(this.user_simulations.indexOf(sim), 1);
+
+	}
+
+
+
+
+	edit_user_info(type) {
+		let value = {};
+		switch (type) {
+			case "name": value = this.user_info.user_name; break;
+			case "email": value = this.user_info.user_email; break;
+			case "phone": value = this.user_info.phone_no; break;
+			case "school": value = this.user_info.school; break;
+			case "degree": value = this.user_info.degree; break;
 		}
 		let alert = this.alertCtrl.create({
-			title: 'Edit your '+type,
+			title: 'Edit your ' + type,
 			inputs: [
 				{
 					name: 'edited_data',
-					placeholder: 'Enter your '+type +' here...',
-					type: type=="phone"?'number':'text',
-					value: ''+ value
+					placeholder: 'Enter your ' + type + ' here...',
+					type: type == "phone" ? 'number' : 'text',
+					value: '' + value
 				}
 			],
 			buttons: [
@@ -120,27 +144,27 @@ export class Profile {
 				{
 					text: 'Edit',
 					handler: data => {
-						switch(type){
-							case "email":this.user_info.user_email=data.edited_data;break;
-							case "phone":this.user_info.phone_no=data.edited_data;break;
-							case "school":this.user_info.school=data.edited_data;break;
-							case "degree":this.user_info.degree=data.edited_data;break;
-							case "name":this.user_info.user_name=data.edited_data;break;
+						switch (type) {
+							case "email": this.user_info.user_email = data.edited_data; break;
+							case "phone": this.user_info.phone_no = data.edited_data; break;
+							case "school": this.user_info.school = data.edited_data; break;
+							case "degree": this.user_info.degree = data.edited_data; break;
+							case "name": this.user_info.user_name = data.edited_data; break;
 						}
-					console.log(this.user_info);
-					// this.http.get("https://ffserver.eu-gb.mybluemix.net/edit-user?user_name="+this.user_info.user_name+"&degree="+this.user_info.degree+"&user_email="+this.user_info.user_email+"&school="+this.user_info.school+"&phone_no="+this.user_info.phone_no+"&user_id="+this.user_info.user_id).subscribe(data => {
-					// 	var res = JSON.parse(data['_body']);
-					// 	// this.user_simulations=res;
-					// 	console.log(res);
-					// 	// this.loading=false;
-					// });
+						console.log(this.user_info);
+						// this.http.get("https://ffserver.eu-gb.mybluemix.net/edit-user?user_name="+this.user_info.user_name+"&degree="+this.user_info.degree+"&user_email="+this.user_info.user_email+"&school="+this.user_info.school+"&phone_no="+this.user_info.phone_no+"&user_id="+this.user_info.user_id).subscribe(data => {
+						// 	var res = JSON.parse(data['_body']);
+						// 	// this.user_simulations=res;
+						// 	console.log(res);
+						// 	// this.loading=false;
+						// });
 
-					this.http.post("https://ffserver.eu-gb.mybluemix.net/edit-user",this.user_info).subscribe(data => {
-						var res = JSON.parse(data['_body']);
-						// this.user_simulations=res;
-						console.log(res);
-						// this.loading=false;
-					});
+						this.http.post("https://ffserver.eu-gb.mybluemix.net/edit-user", this.user_info).subscribe(data => {
+							var res = JSON.parse(data['_body']);
+							// this.user_simulations=res;
+							console.log(res);
+							// this.loading=false;
+						});
 
 					}
 				}
@@ -149,58 +173,58 @@ export class Profile {
 		alert.present();
 	}
 
-		showHead(refresher) {
-			this.refresher=refresher;
-			setTimeout(() => {
-				refresher.complete();
-			}, 5000);
+	showHead(refresher) {
+		this.refresher = refresher;
+		setTimeout(() => {
+			refresher.complete();
+		}, 5000);
+	}
+	hideHead() {
+		if (this.refresher)
+			this.refresher.complete();
+	}
+
+
+
+	logout() {
+
+		this.store.set('user_id', "");
+		localStorage.setItem('expo', null);
+		this.fb.logout();
+		this.app.getRootNav().setRoot(LoginPage);
+		//root.popToRoot();
+
+	}
+
+	addTimer(date) {
+
+		// console.log("in add timer",date)
+		this.StartDate = new Date(date)
+		// console.log('date',this.StartDate);
+		this.StartDate.setMilliseconds(0);
+		this.timercal();
+	}
+
+	status(sim) {
+		if (sim.status === "pending payment") {
+			this.addTimer(sim.acceptance_deadline);
+			return true;
 		}
-		hideHead(){
-			if(this.refresher)
-				this.refresher.complete();
-		}
-
-
-
-		logout(){
-
-this. store.set('user_id', "");
-localStorage.setItem('expo',null);
-this.fb.logout();
-this.app.getRootNav().setRoot(LoginPage);
-//root.popToRoot();
-
-		}
-	
-		addTimer(date){
-			
-			 // console.log("in add timer",date)
-			  this.StartDate=new Date(date)
-			 // console.log('date',this.StartDate);
-			  this.StartDate.setMilliseconds(0);	
-			  this.timercal();
-		}
-
-		status(sim){
-			if(sim.status==="pending payment"){
-				this.addTimer(sim.acceptance_deadline);
-				return true;
-			}
-		}
-		timercal(){
-			let dump =new Date();
+	}
+	timercal() {
+		let dump = new Date();
 		//	dump.setHours(dump.getHours()-2); 
-			//this.StartDate = new Date(dump.getTime() + 24 * 60 * 60 * 1000);
+		//this.StartDate = new Date(dump.getTime() + 24 * 60 * 60 * 1000);
 		//	console.log("INNNN",this.StartDate);
-			dump.getTimezoneOffset();
-			let diff=this.StartDate.getTime() - dump.getTime();
-			var timeDiff = Math.abs(diff);
-			this. diffDays = Math.floor(timeDiff / (1000 * 3600 * 24)); 
-			this. diffhours =Math.floor((timeDiff-this.diffDays*1000*3600*24)/(1000*3600)) ; 
-			this.diffmins= Math.floor(((timeDiff-this.diffDays*1000*3600*24)-this.diffhours*1000*3600) /(1000 * 60) ) ;
-		  
-		    this.diffhours+=(this.diffDays*24);
-		
-		}
+		dump.getTimezoneOffset();
+		let diff = this.StartDate.getTime() - dump.getTime();
+		var timeDiff = Math.abs(diff);
+		this.diffDays = Math.floor(timeDiff / (1000 * 3600 * 24));
+		this.diffhours = Math.floor((timeDiff - this.diffDays * 1000 * 3600 * 24) / (1000 * 3600));
+		this.diffmins = Math.floor(((timeDiff - this.diffDays * 1000 * 3600 * 24) - this.diffhours * 1000 * 3600) / (1000 * 60));
+
+		this.diffhours += (this.diffDays * 24);
+
+	}
 
 }
--- src/pages/tabs/tabs.ts
+++ src/pages/tabs/tabs.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 import { FieldsPage } from '../fields/fields';
 import { Videos } from '../videos/videos';
 import { Profile } from "../profile/profile";
-import {AddSimulationPage } from "../add-simulation/add-simulation"
+import { AddSimulationPage } from "../add-simulation/add-simulation"
 import { ActionSheetController, Platform, LoadingController } from 'ionic-angular';
 import { NavController, NavParams } from 'ionic-angular';
 import { AlertController } from 'ionic-angular';
@@ -23,15 +23,15 @@ import { ChooseCompanyPage } from '../choose-company/choose-company';
 export class TabsPage {
 	company_or_not: any;
 	superAdmin;
-	expo:any;
+	expo: any;
 	tab1Root = FieldsPage;
 	tab2Root = Videos;
 	tab3Root = Profile;
 	//tab5Root = ExpotimerPage;
 	ios: boolean = false;
 	root;
-	companies=[];
-	connection_error_popup:any;
+	companies = [];
+	connection_error_popup: any;
 	constructor(public actionSheetCtrl: ActionSheetController, public navCtrl: NavController, navParams: NavParams, public http: Http, private store: Storage, public plt: Platform, public alertCtrl: AlertController, private network: Network, private loadingCtrl: LoadingController) {
 		this.network.onDisconnect().subscribe(() => {
 			this.connection_error_popup = this.loadingCtrl.create({
@@ -44,29 +44,29 @@ export class TabsPage {
 			this.connection_error_popup.dismiss();
 		});
 		this.store.get('user_id').then((val) => {
-			
-					  
-					http.get("https://ffserver.eu-gb.mybluemix.net/user_info?id="+val).subscribe(data => {
-						var res = JSON.parse(data['_body']);
-						this.superAdmin=res.company_or_not;
-						console.log(this.superAdmin);
-						//this.loading=false;
-					});
-				});
 
-				http.get("https://ffserver.eu-gb.mybluemix.net/all-companies?id=").subscribe(data => {
-					var res = JSON.parse(data['_body']);
-					this.companies=res;
-					console.log(this.companies);
-					//this.loading=false;
-				});		
+
+			http.get("https://ffserver.eu-gb.mybluemix.net/user_info?id=" + val).subscribe(data => {
+				var res = JSON.parse(data['_body']);
+				this.superAdmin = res.company_or_not;
+				console.log(this.superAdmin);
+				//this.loading=false;
+			});
+		});
+
+		http.get("https://ffserver.eu-gb.mybluemix.net/all-companies?id=").subscribe(data => {
+			var res = JSON.parse(data['_body']);
+			this.companies = res;
+			console.log(this.companies);
+			//this.loading=false;
+		});
 		this.root = this.tab1Root;
 		this.company_or_not = localStorage.getItem('company_or_not');
 		if (plt.is('ios')) {
 			this.ios = true;
 		}
 		//this.expo= localStorage.getItem('expo');
-	
+
 	}
 	switch1() {
 		this.root = this.tab1Root;
@@ -80,7 +80,7 @@ export class TabsPage {
 	switch4() {
 		this.presentAdminActionSheet();
 	}
-	
+
 
 	presentAdminActionSheet() {
 		let actionSheet = this.actionSheetCtrl.create({
@@ -92,13 +92,13 @@ export class TabsPage {
 						console.log('17 Jan, 11:00 AM');
 						this.navCtrl.push(AddSimulationPage);
 						console.log(this.company_or_not);
-						
+
 					}
 				},
 				{
 					text: 'Edit Profile',
 					handler: () => {
-						this.navCtrl.push(EditCompanyPage,this.company_or_not);
+						this.navCtrl.push(EditCompanyPage, this.company_or_not);
 						console.log('20 Feb, 7:300 AM');
 					}
 				},
@@ -113,10 +113,10 @@ export class TabsPage {
 				{
 					text: 'Accept Applicants',
 					handler: () => {
-					this.navCtrl.push(AcceptapplicantsPage);
+						this.navCtrl.push(AcceptapplicantsPage);
 					}
 				},
-				
+
 				{
 					text: 'Cancel',
 					role: 'cancel',
@@ -125,26 +125,26 @@ export class TabsPage {
 					}
 				}
 			]
-				
-		});
-		if(this.superAdmin==-2){
-		actionSheet.addButton	({
-			text: 'Choose Company',
-			
-			handler: () => {
 
-				setTimeout(() => {
-				this.ChooseCompanyList();
-					
-			}, 100); 
-			}
-			
 		});
+		if (this.superAdmin == -2) {
+			actionSheet.addButton({
+				text: 'Choose Company',
+
+				handler: () => {
+
+					setTimeout(() => {
+						this.ChooseCompanyList();
+
+					}, 100);
+				}
+
+			});
 		}
 		actionSheet.present();
 	}
-	
-	ChooseCompanyList(){
+
+	ChooseCompanyList() {
 		let alert = this.alertCtrl.create();
 		alert.setTitle('Select Gender');
 		for (var index = 0; index < this.companies.length; index++) {
@@ -154,17 +154,17 @@ export class TabsPage {
 				label: element.company_name,
 				value: element.company_id,
 				checked: false
-			  });	
+			});
 		}
-		
-	
+
+
 		alert.addButton('Cancel');
 		alert.addButton({
-		  text: 'OK',
-		  handler: data => {
-			this.company_or_not=data;
-			localStorage.setItem('company_or_not', this.company_or_not);
-			console.log("hya de",data);
+			text: 'OK',
+			handler: data => {
+				this.company_or_not = data;
+				localStorage.setItem('company_or_not', this.company_or_not);
+				console.log("hya de", data);
 			}
 		});
 		alert.present();
@@ -190,8 +190,8 @@ export class TabsPage {
 					placeholder: 'Enter 1 for video 0 for image...',
 					type: 'text'
 				},
-				
-			
+
+
 			],
 			buttons: [
 				{
@@ -205,15 +205,15 @@ export class TabsPage {
 					text: 'Add',
 					handler: data => {
 
-						console.log("ana goa and this is the data :",data) ; 
-						var videoOrNot = 0  ; 
-						if (data.video_or_not =="1")
-							videoOrNot = 1 ; 
+						console.log("ana goa and this is the data :", data);
+						var videoOrNot = 0;
+						if (data.video_or_not == "1")
+							videoOrNot = 1;
 						var video = {
 							"video_link": data.video_link,
 							"description": data.video_description,
-							"company_id": this.company_or_not, 
-							"video_or_not": videoOrNot 
+							"company_id": this.company_or_not,
+							"video_or_not": videoOrNot
 						}
 						console.log(video);
 						this.http.post("https://ffserver.eu-gb.mybluemix.net/add-video", video).subscribe(data => {
